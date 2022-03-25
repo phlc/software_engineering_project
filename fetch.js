0097c64335d6510f98f35aa1eb05377601080f87
phlc <pedrocarvalho@outlook.com>
2022-03-25 12:48:49 -0300

    Fetch from openlibrary

---
+++ fetch.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const query = async function (queryString) {
+  const jsonData = await fetch(
+    `http://openlibrary.org/search.json?q=${queryString}`
+  ).then((response) => response.json());
+
+  console.log(jsonData);
+
+  const books = jsonData["docs"].map(function (elem) {
+    const book = {
+      titulo: elem["title"],
+      autor: elem["author_name"],
+      editora: elem["publisher"],
+      chave: elem["key"].split("/")[2],
+      capa: `https://covers.openlibrary.org/b/olid/${
+        elem["key"].split("/")[2]
+      }-M.jpg`,
+    };
+    return book;
+  });
+  return books;
+};
+
+const main = async function () {
+  const data = await query("Pedro Bial");
+  console.log(data);
+};
+
+main();
