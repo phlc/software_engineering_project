efa7debd51c3d5e74c5c265602372c82b3ae19b2
Larissa-D-Gomes <larissadgomes2001@gmail.com>
2022-04-16 16:13:27 -0300

    Diretório back-end -> Spring boot OK

---
+++ backend/LPSBookStore/LPSBookStore/src/main/java/com/LPSBookStore/LPSBookStore/Entities/Client.java
@@ -0,0 +1,64 @@
+package com.LPSBookStore.LPSBookStore.Entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CLIENT")
+public class Client {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String email;
+	private String name;
+	private String cpf;
+	private String password;
+	
+	public Client() {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
---
+++ backend/LPSBookStore/LPSBookStore/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO CLIENT (CPF, EMAIL, NAME, PASSWORD) VALUES 
+('345.654.268.98', 'ana@gmail.com', 'Ana Laura', 'NDIHDUG'),
+('123.654.908.99', 'larissa@gmail.com', 'Larissa Gomes', 'SADNADKJ'),
+('331.604.218.76', 'pedrocarvalho@gmail.com', 'Pedro Carvalho', 'JFGIEHRI'),
+('912.086.245.35', 'pedroreis@gmail.com', 'Pedro Reis', 'DFNIJGEIRGN'),
+('122.432.123.45', 'tarcila@gmail.com', 'Tárcila Fernanda', 'OIEWIRHFD');
