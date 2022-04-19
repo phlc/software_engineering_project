625b570b6fd5c76eab3855d35164b453dd3c8814
Ana Laura Fernandes <analaura.fernandes@dtidigital.com.br>
2022-04-19 09:44:30 -0300

    Continuação da implementação da tela de detalhes dos livros

--- frontend/src/Pages/Details/index.tsx
+++ frontend/src/Pages/Details/index.tsx
@@ -1,20 +1,42 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import BookCard from "../../Components/BookCard";
 import { Menu } from "../../Components/Menu/Menu";
 import { Book } from "../../types";
 import { bookMock } from "../../__mock__/mock";
-import { Background, CoverImage, MainContainer, SectionTitle } from "./styles";
+import ArrowForward from '@material-ui/icons/ArrowForward';
+import { AuthorText, Background, BookTitle, CoverImage, DetailsContainer, LinkStyle, MainContainer, Row, SectionTitle, SinopseContainer, SubText } from "./styles";
 
 export default function BookDetails() {
     let params = useParams();
     //obter livro pelo id. por enquanto usando objeto mockado
     const book = bookMock.filter((b: Book) => b.id.toString() === params.bookId)[0];
+    //obter livros do autor pela nome do mesmo. por enquanto usando objeto mockado
+    const sameAuthorBooks = bookMock.filter((b: Book) => b.author === book.author);
     return(
-        <Background>
+        <>
             <Menu />
-            <SectionTitle>Sobre o livro</SectionTitle>
-            <MainContainer>
-                <CoverImage src={book.coverURL} />
-            </MainContainer>
-        </Background>
+            <Background>
+                <SectionTitle>Sobre o livro</SectionTitle>
+                <MainContainer>
+                    <CoverImage src={book.coverURL} />
+                    <DetailsContainer>
+                        <BookTitle>{book.title}</BookTitle>
+                        <AuthorText>{book.author}</AuthorText>
+                        <SubText>{book.description}</SubText>
+                        <Link to="/sinopse" style={LinkStyle}>Ver a sinopse completa  <ArrowForward fontSize="small"style={{ marginBottom: -5}}/></Link>
+                    </DetailsContainer>
+                </MainContainer>
+                <SectionTitle>Mais livros de {book.author}</SectionTitle>
+                <Row>
+                    {sameAuthorBooks.map((b: Book) => <BookCard book={b} />)}
+                </Row>
+                <SectionTitle>Sinopse Completa</SectionTitle>
+                <SinopseContainer>
+                    <SubText>
+                        {book.description}
+                    </SubText>
+                </SinopseContainer>
+            </Background>
+        </>
     );
 }
--- frontend/src/Pages/Details/styles.ts
+++ frontend/src/Pages/Details/styles.ts
@@ -3,19 +3,52 @@ import '../../assets/fonts/Fonts.css';
 
 export const Background = styled.div`
     background-color: #EFEFEF;
+    padding: 30px 70px;
 `;
 
 export const SectionTitle = styled.text`
     display: flex;
     font-family: 'Montserrat-bold';
     font-size: 26px;
-    margin: 30px 70px;
+    margin: 30px 0;
+`;
+
+export const BookTitle = styled.text`
+    display: flex;
+    font-family: 'Montserrat-bold';
+    font-size: 28px;
+    margin: 30px 0 10px 30px;
+`;
+
+export const DetailsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+`;
+
+export const AuthorText = styled.text`    
+    display: flex;
+    font-family: 'Montserrat-regular';
+    font-size: 20px;
+    margin: 0 0 10px 30px;
+`;
+
+export const SubText = styled.text`    
+    display: flex;
+    font-family: 'Montserrat-regular';
+    font-size: 16px;
+    margin: 0 0 10px 30px;
+`;
+
+export const Row = styled.div`
+    display: flex;
+    flex-direction: row;
 `;
 
 export const MainContainer = styled.section`
     display: flex;
     background-color: #FFFFFF;
-    margin: 20px 70px;
+    margin: 20px 0;
     height: 351px;
     padding: 36px;
     border-radius: 10px;
@@ -29,3 +62,21 @@ export const CoverImage = styled.img`
     height: 347px;
     align-self: flex-start;
 `;
+
+export const SinopseContainer = styled.section`
+    display: flex;
+    padding: 36px;
+    margin: 20px 0;
+    text-align: left;
+    background-color: #FFFFFF;
+    border-radius: 10px;
+`;
+
+export const LinkStyle = {
+    textDecoration: 'none',
+    fontFamily: 'Montserrat-Bold',
+    fontSize: 16,
+    marginLeft: 30,
+    marginTop: 10,
+    color: '#000000'
+};
