eeece74d76114a0fea2b9f9718dafa2a37f5d999
Ana Laura Fernandes <analaura.fernandes@dtidigital.com.br>
2022-04-18 21:49:42 -0300

    Adição de arquivo de objetos mock

--- frontend/src/Pages/Home/index.tsx
+++ frontend/src/Pages/Home/index.tsx
@@ -1,81 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Menu } from "../../Components/Menu/Menu"
 import { Book } from "../../types"
+import { bookMock } from "../../__mock__/mock"
 import Categories from "../Categories"
 import Header from "../Header"
 import Releases from "../Releases"
 import { Background } from "./styles"
 
 export default function Home () {
-
-    const bookMock: Book[] = [{
-        coverURL: 'https://m.media-amazon.com/images/I/5144Ghp-b8L.jpg',
-        rate: 4,
-        title: 'A sutil arte de ligar o foda-se',
-        author: 'Mark Manson',
-        description: 'Chega de tentar buscar um sucesso que só existe na sua cabeça. Chega de se torturar para pensar positivo enquanto sua vida vai ladeira abaixo. Chega de se sentir inferior por não ver o lado bom de estar no fundo do poço.',
-        price: 20.93,
-        stock: 4,
-        category: 'Auto Ajuda',
-        releaseDate: '20/01/1995',
-        isRelease: true,
-    },{
-        coverURL: 'https://m.media-amazon.com/images/I/5144Ghp-b8L.jpg',
-        rate: 4,
-        title: 'A sutil arte de ligar o foda-se',
-        author: 'Mark Manson',
-        description: 'Chega de tentar buscar um sucesso que só existe na sua cabeça. Chega de se torturar para pensar positivo enquanto sua vida vai ladeira abaixo. Chega de se sentir inferior por não ver o lado bom de estar no fundo do poço.',
-        price: 20.93,
-        stock: 4,
-        category: 'Auto Ajuda',
-        releaseDate: '20/01/1995',
-        isRelease: true,
-    },{
-        coverURL: 'https://m.media-amazon.com/images/I/5144Ghp-b8L.jpg',
-        rate: 4,
-        title: 'A sutil arte de ligar o foda-se',
-        author: 'Mark Manson',
-        description: 'Chega de tentar buscar um sucesso que só existe na sua cabeça. Chega de se torturar para pensar positivo enquanto sua vida vai ladeira abaixo. Chega de se sentir inferior por não ver o lado bom de estar no fundo do poço.',
-        price: 20.93,
-        stock: 4,
-        category: 'Auto Ajuda',
-        releaseDate: '20/01/1995',
-        isRelease: true,
-    },{
-        coverURL: 'https://m.media-amazon.com/images/I/5144Ghp-b8L.jpg',
-        rate: 4,
-        title: 'A sutil arte de ligar o foda-se',
-        author: 'Mark Manson',
-        description: 'Chega de tentar buscar um sucesso que só existe na sua cabeça. Chega de se torturar para pensar positivo enquanto sua vida vai ladeira abaixo. Chega de se sentir inferior por não ver o lado bom de estar no fundo do poço.',
-        price: 20.93,
-        stock: 4,
-        category: 'Auto Ajuda',
-        releaseDate: '20/01/1995',
-        isRelease: true,
-    },{
-        coverURL: 'https://m.media-amazon.com/images/I/5144Ghp-b8L.jpg',
-        rate: 4,
-        title: 'A sutil arte de ligar o foda-se',
-        author: 'Mark Manson',
-        description: 'Chega de tentar buscar um sucesso que só existe na sua cabeça. Chega de se torturar para pensar positivo enquanto sua vida vai ladeira abaixo. Chega de se sentir inferior por não ver o lado bom de estar no fundo do poço.',
-        price: 20.93,
-        stock: 4,
-        category: 'Auto Ajuda',
-        releaseDate: '20/01/1995',
-        isRelease: true,
-    },{
-        coverURL: 'https://m.media-amazon.com/images/I/5144Ghp-b8L.jpg',
-        rate: 4,
-        title: 'A sutil arte de ligar o foda-se',
-        author: 'Mark Manson',
-        description: 'Chega de tentar buscar um sucesso que só existe na sua cabeça. Chega de se torturar para pensar positivo enquanto sua vida vai ladeira abaixo. Chega de se sentir inferior por não ver o lado bom de estar no fundo do poço.',
-        price: 20.93,
-        stock: 4,
-        category: 'Auto Ajuda',
-        releaseDate: '20/01/1995',
-        isRelease: true,
-    }]
-
     return (
         <Background>
             <Header />
